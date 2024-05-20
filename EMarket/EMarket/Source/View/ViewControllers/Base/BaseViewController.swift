aa4ea231efb6054c5fe05e5b3262bce8f408ba17
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-05-20 20:21:07 +0300

    BaseViewController and ViewModel has been created.

--- EMarket/EMarket/Source/View/ViewControllers/Base/BaseViewController.swift
+++ EMarket/EMarket/Source/View/ViewControllers/Base/BaseViewController.swift
@@ -12,5 +12,42 @@ open class BaseViewController: UIViewController {
 
     override open func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .viewControllerBackground
+        navigationController?.navigationBar.prefersLargeTitles = false
+    }
+}
+
+// MARK: - Public Methods
+extension BaseViewController {
+    func showAlert(
+        title: String,
+        description: String,
+        possitiveButtonText: String,
+        negativeButtonText: String? = nil,
+        onClickedPossitveButton: (() -> Void)? = nil,
+        onClickedNegativeButton: (() -> Void)? = nil
+    ) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+            
+            let possitiveAction = UIAlertAction(title: possitiveButtonText, style: .default) { _ in
+                onClickedPossitveButton?()
+            }
+            
+            alert.addAction(possitiveAction)
+            if let negativeButtonText = negativeButtonText {
+                let negativeAction = UIAlertAction(title: negativeButtonText, style: .cancel) { _ in
+                    onClickedNegativeButton?()
+                }
+                alert.addAction(negativeAction)
+            }
+            
+            alert.view.tintColor = .blue900
+            self.present(alert, animated: true)
+        }
     }
 }
---
+++ EMarket/EMarket/Source/ViewModel/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  EMarket
+//
+//  Created by Cemal on 20.05.2024.
+//
+
+import Foundation
+
+// MARK: - ViewModel
+class ViewModel {
+    
+}
