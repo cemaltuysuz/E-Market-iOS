4e5baeab87b7af097cd8cc39eb8d6c6b2267220d
Cemal <ctuysuz18@posta.pau.edu.tr>
2024-05-21 11:15:01 +0300

    Constants created.

---
+++ EMarket/EMarket/Source/Core/Constant/Constant+Defaults.swift
@@ -0,0 +1,13 @@
+//
+//  Constant+Defaults.swift
+//  EMarket
+//
+//  Created by Cemal on 21.05.2024.
+//
+
+import Foundation
+
+// MARK: - Constant+Defaults
+extension Constant.Defaults {
+    static let pageTitle = "E-Market"
+}
---
+++ EMarket/EMarket/Source/Core/Constant/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  EMarket
+//
+//  Created by Cemal on 21.05.2024.
+//
+
+import Foundation
+
+// MARK: - Constant
+enum Constant {
+    enum Defaults {}
+}
